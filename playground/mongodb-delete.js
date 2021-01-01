88e4d123743717c6bea5d1ec67e90cf0535a2823
AHViewpager <AHViewpager@gmail.com>
2021-01-01 17:04:28 +0330

    delete in mongodb

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = "mongodb://localhost:27017/TodoApp"
+const dbname = "TodoApp"
+
+MongoClient.connect(url,(error, client) => {
+
+    if (error) { return console.log("Unable to connect mongodb"); }
+
+    console.log("Connected to Mongodb");
+
+    const db = client.db(dbname)
+
+    // db.collection('Todos').deleteMany({completed: false}).then((result) => {
+    //     console.log("Todos Deleted Successfully");
+    // }, (error) => {
+    //     console.log("an error ouccurred while deleting todo");
+    // })
+
+    // db.collection('Todos').deleteOne({text: 'Do Something'}).then((result) => {
+    //     console.log("Todo Deleted Successfully");
+    // }, (error) => {
+    //     console.log("an error ouccurred while deleting todo");
+    // })
+
+    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log("Todo Deleted Successfully");
+    }, (error) => {
+        console.log("an error ouccurred while deleting todo");
+    })
+
+    client.close()
+
+})
