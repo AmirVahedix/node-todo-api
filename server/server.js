6b5293c43d18d1db42088d7c61179458ade97d51
amirvahedix <amirvahedix@gmail.com>
2021-01-03 10:48:52 +0330

    create GET method for todos api

--- server/server.js
+++ server/server.js
@@ -10,17 +10,22 @@ app.use(bodyParser.json())
 
 
 app.post('/todos', (request, response) => {
-    
     let todo = new Todo({
         text: request.body.text
     })
-
     todo.save().then((doc) => {
         response.status(500).send(doc)
     }, (e) => {
         response.status(400).send(e)
     })
+})
 
+app.get('/todos', (request, response) => {
+    Todo.find().then((todos) => {
+        response.send(todos)
+    }, (e) => {
+        response.status(400).send(todos)
+    })
 })
 
 
